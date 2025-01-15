dfeff6154c4b3a1ab287c021aee0f2bfae57d8ed
Rob van der Wouw <2452067+rvanderwouw@users.noreply.github.com>
2025-01-15 14:49:33 +0100

    Update __init__.py
    Changed timer_count_down_setter from 3 to 4 bytes to accomodate firmware v0.8

--- ecoventv2/__init__.py
+++ ecoventv2/__init__.py
@@ -677,7 +677,7 @@ class Fan(object):
 
     @filter_timer_countdown.setter
     def filter_timer_countdown(self, input ):
-        val = int(input,16).to_bytes(3,'big')
+        val = int(input,16).to_bytes(4,'big')
         self._filter_timer_countdown = str ( val[2] ) + "d " +str ( val[1] ) + "h " + str ( val[0] ) + "m " 
 
     @property
